f8e84c463b95250c650d04cf240635582774bfdd
Lukas <lukvonstrom@web.de>
2017-04-10 17:31:36 +0200

    [~] Fixes Lint Error
    Caused by unused import.

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,6 @@
 
 // Include dependencies
 import * as express from 'express';
-import * as path from 'path';
 import * as logger from 'morgan';
 import * as favicon from 'serve-favicon';
 import * as cookieParser from 'cookie-parser';
